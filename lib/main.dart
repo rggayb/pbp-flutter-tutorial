f4787f4c335c77589d8d25cee6960f1405c420bd
Rangga Yudhistira <ranggabbrata@gmail.com>
2022-11-17 15:04:48 +0700

    lab 8 done!

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,33 @@
-import 'form.dart';
 import 'package:flutter/material.dart';
+import 'package:lab_flutter/page/form.dart';
+import 'package:lab_flutter/page/to_do_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+  const MyApp({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
-    const MyHomePage({super.key});
+  const MyHomePage({super.key});
 
-    final String title = 'Flutter Demo Home Page';
+  final String title = 'Flutter Demo Home Page';
 
-    @override
-    State<MyHomePage> createState() => _MyHomePageState();
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -53,10 +54,9 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      // Menambahkan drawer menu
       drawer: Drawer(
         child: Column(
           children: [
@@ -81,10 +81,19 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               },
             ),
+            ListTile(
+            title: const Text('To Do'),
+            onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const ToDoPage()),
+                );
+            },
+        ),
           ],
         ),
       ),
-
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
